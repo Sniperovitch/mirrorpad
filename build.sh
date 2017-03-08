301ea19be30d9e7b39d45fff9545f5bb0018868f
David Sniperovitch <david@sniperovitch.fr>
2017-03-08 01:34:42 +0100

    certif pour TLS

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 apt-get -qq update
-apt-get install -y libconfig-yaml-perl libwww-perl liblwp-protocol-https-perl libencode-perl liburi-encode-perl
+apt-get install -y libconfig-yaml-perl libwww-perl liblwp-protocol-https-perl libencode-perl liburi-encode-perl ca-certificates
 
