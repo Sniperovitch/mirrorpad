aa67af590ec06d8222137243d521d49b9e3bfbba
David Sniperovitch <david@sniperovitch.fr>
2017-03-12 14:48:15 +0100

    install des modules avec cpanm

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-apt-get -qq update
-apt-get install -y libconfig-yaml-perl libwww-perl liblwp-protocol-https-perl libencode-perl liburi-encode-perl ca-certificates
+#apt-get -qq update
+#apt-get install -y libconfig-yaml-perl libwww-perl liblwp-protocol-https-perl libencode-perl liburi-encode-perl ca-certificates
 cp -v mirrorpad.yml.smp mirrorpad.yml
-/home/travis/perl5/perlbrew/bin/cpanm Etherpad
-
+/home/travis/perl5/perlbrew/bin/cpanm Config::YAML Encode URI::Encode LWP::UserAgent Etherpad
